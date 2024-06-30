e38bb38112db985abf406b1600bcdb1c2da1c152
grigor-dochev <48100639+grigor-dochev@users.noreply.github.com>
2024-06-30 23:42:39 +0200

    Add WatchOS app to target

---
+++ MindfulPacer/WatchOS/WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSApp.swift
+//  WatchOS Watch App
+//
+//  Created by Grigor Dochev on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
